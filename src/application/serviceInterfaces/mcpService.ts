446f0269ce6c443a456a292ee25b8f7e782edd15
Oriol Orbea <oriol.orbea@gmail.com>
2026-06-25 17:51:40 +0200

    feat: add callTool method to IMcpService interface for tool invocation

--- src/application/serviceInterfaces/mcpService.ts
+++ src/application/serviceInterfaces/mcpService.ts
@@ -1,5 +1,6 @@
-import { McpTool } from "../../domain";
+import { McpTool, McpToolResult } from "../../domain";
 
 export interface IMcpService {
     discoverTools(): Promise<McpTool[]>;
+    callTool(name: string, args?: Record<string, unknown>): Promise<McpToolResult>;
 }
