e2a9bdc455f644ac272b8c9888cb8731ce10c79f
Oriol Orbea <oorbea@pasiona.com>
2026-06-26 12:06:04 +0200

    refactor: change DomainError class to abstract and protect constructor

--- src/domain/exceptions/domainError.ts
+++ src/domain/exceptions/domainError.ts
@@ -1,5 +1,5 @@
-export class DomainError extends Error {
-  constructor(message: string) {
+export abstract class DomainError extends Error {
+  protected constructor(message: string) {
     super(message);
     Object.setPrototypeOf(this, new.target.prototype);
     this.name = this.constructor.name;
