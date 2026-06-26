47e5ca3e4ba395b8ffa9188c0fe5056441ed345e
Oriol Orbea <oorbea@pasiona.com>
2026-06-26 10:22:17 +0200

    feat: add OffboardingStateEnum for managing offboarding process states

---
+++ src/domain/enums/index.ts
@@ -0,0 +1,1 @@
+export { OffboardingStateEnum } from './offboardingState';
---
+++ src/domain/enums/offboardingState.ts
@@ -0,0 +1,6 @@
+export enum OffboardingStateEnum {
+    NotStarted,
+    InProgress,
+    PendingRevision,
+    Finished
+}
