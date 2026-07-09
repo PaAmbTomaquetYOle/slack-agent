579b77720af4938717782c10658bbcda189f9b04
Oriol Orbea <oorbea@pasiona.com>
2026-07-09 11:04:26 +0200

    feat: add AuthenticationRequiredError for handling authentication requirements

--- src/domain/exceptions/index.ts
+++ src/domain/exceptions/index.ts
@@ -1,3 +1,4 @@
 export { DomainError } from './domainError';
 export { InvalidStateTransitionError } from './invalidStateTransitionError';
 export { InvalidValueObjectError } from './invalidValueObjectError';
+export { AuthenticationRequiredError } from './authenticationRequiredError';
